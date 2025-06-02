bec4eab6c634fac00f3a908e1eb37e97b0cc6655
my-nxt-learnings <mynxtlearnings2025@gmail.com>
2025-06-02 18:55:25 +0530

    updated Welcome.jsx

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import "../styles/Welcome.css";
+import "../styles/welcome.css";
 
 const Welcome = () => {
   const navigate = useNavigate();
